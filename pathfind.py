f73a4f5448c60237f47a77d3269aae34a6fc1c9f
Yassate <bartosz.walawender@gmail.com>
2023-01-21 23:42:19 +0100

    Added buttons, not fully functional yet

--- pathfind.py
+++ pathfind.py
@@ -5,19 +5,57 @@ from AStar import AreaGrid
 from storage_types import Location
 
 def draw_window(mygrid):
-    WIN.fill(LGRAY)
+    WIN.fill(LGREY)
     mygrid.draw()
-    textfont = pygame.font.SysFont("monospace", 20)
-    hello_text = textfont.render("Hola Amigo", 1, WHITE)
-    WIN.blit(hello_text, (810, 20))
-    pygame.display.update()
+    # pygame.display.update()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 WHITE = (255, 255, 255)
-LGRAY = (180, 180, 180)
+DGREEN = (24, 148, 92)
+LGREY = (180, 180, 180)
+DGREY = (100, 100, 100)
+DDGREY = (70, 70, 70)
+
 FPS = 30
 CITIES_COUNT = 10
+
+class Button:
+    def __init__(self, text, width, height, pos):
+        self.color = DGREY
+        self.font = pygame.font.Font(None, round(4*height/5))
+        self.text_surf = self.font.render(text, True, WHITE)
+        self.button_pressed = False
+        self.mouse_pressed = False
+        self.pos_y_default = pos[1]
+        self.pos_y_pressed = pos[1] + round(height/6)
+
+        self.top_rect = pygame.Rect(pos, (width, height))
+        self.bottom_rect = pygame.Rect((pos[0], pos[1] + round(height/6)), (width, height))
+        self.text_rect = self.text_surf.get_rect()
+
+    def draw(self):
+        self.click_detect()
+        self._update_button_pos()
+        pygame.draw.rect(WIN, DDGREY, self.bottom_rect, border_radius=12)
+        pygame.draw.rect(WIN, self.color, self.top_rect, border_radius=12)
+        pygame.draw.rect(WIN, WHITE, self.top_rect, width=1, border_radius=12)
+        WIN.blit(self.text_surf, self.text_rect)
+
+    def _update_button_pos(self):
+        self.top_rect.y = self.pos_y_pressed if self.button_pressed else self.pos_y_default
+        self.color = DGREEN if self.button_pressed else DGREY
+        self.text_rect.center = self.top_rect.center
+
+    def click_detect(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(mouse_pos):
+            if(pygame.mouse.get_pressed()[0]):
+                self.mouse_pressed = True
+            else:
+                if self.mouse_pressed == True:
+                    self.button_pressed = not self.button_pressed
+                    self.mouse_pressed = False
 
 def main():
     clock = pygame.time.Clock()
@@ -25,6 +63,10 @@ def main():
     pygame.font.init()
 
     mygrid = AreaGrid(win=WIN, filepath="input_files/obstacles.txt", wh_pix=(WIDTH-200, HEIGHT))
+    button1 = Button('Tomato', 160 ,30, (820,10))
+    button2 = Button('Banana', 160 ,30, (820,50))
+    button3 = Button('Oil', 160 ,30, (820,90))
+    button4 = Button('Potatoes', 160 ,30, (820,130))
 
     #Acceptance criteria - FLOW:
     #Opening the app - you see the map and on the right side, short list of clickable product names + find path and reset button
@@ -33,7 +75,7 @@ def main():
     # "Find Path" triggers algorithm and shows path on the map
     # "Reset" causes deselecting of all products and clears the map
 
-    #TODO #1 - create buttons on the right, pixel art, clickable buttons, like on youtube video 8SzTzvrWaAA
+    #TODO #1 - create multiple buttons on the right, pixel art, clickable buttons, like on youtube video 8SzTzvrWaAA
 
     locations = []
     locations.append(Location(2, 2, "Tomato"))
@@ -73,7 +115,14 @@ def main():
         clock.tick(FPS)
         mygrid.solve_for_locations(*location_pairs[i])
         draw_window(mygrid)
-        pygame.time.wait(500)
+        button1.draw()
+        button2.draw()
+        button3.draw()
+        button4.draw()
+
+        pygame.display.update()
+
+        pygame.time.wait(10)
 
         i += 1
         # mygrid.reset_grid()
